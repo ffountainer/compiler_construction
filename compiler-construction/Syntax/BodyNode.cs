aec533a01939d5e8029b96e7130567a910b7278a
veronika_levasheva <v.levasheva@innopolis.university>
2025-10-16 02:03:56 +0300

    feat(syntax): Implement if stateents

---
+++ compiler-construction/Syntax/BodyNode.cs
@@ -0,0 +1,30 @@
+using compiler_construction.Tokenization;
+using compiler_construction.Tokenization.Keywords;
+
+namespace compiler_construction.Syntax;
+
+public class BodyNode : TreeNode
+{
+    public override string GetName()
+    {
+        return "Body";
+    }
+
+    override public void ReadTokens(out Token lastToken)
+    {
+        lastToken = null;
+        var token = firstToken;
+        bool hasStatements = false;
+        while (token is not End)
+        {
+            children.Add(NodeFactory.ConstructNode(new StatementNode(), lexer, token, out lastToken));
+            hasStatements = true;
+            token = lexer.GetNextToken();
+        }
+
+        if (!hasStatements)
+        {
+            throw new Exception("Empty body is not supported.");
+        }
+    }
+}
--- compiler-construction/Syntax/IfNode.cs
+++ compiler-construction/Syntax/IfNode.cs
@@ -1,17 +1,63 @@
 using compiler_construction.Tokenization;
+using compiler_construction.Tokenization.Keywords;
+using compiler_construction.Tokenization.Symbols;
 
 namespace compiler_construction.Syntax;
 
 public class IfNode : TreeNode
 {
+    private bool isShort;
+    
     public override string GetName()
     {
-        // TODO: Return name based on whether short
-        throw new NotImplementedException();
+        return isShort ? "IfShort" : "If";
     }
 
     public override void ReadTokens(out Token lastToken)
     {
-        throw new NotImplementedException();
+        var token = lexer.GetNextToken();
+        children.Add(NodeFactory.ConstructNode(new ExpressionNode(), lexer, token, out var thenOrArrow));
+
+        if (thenOrArrow is Then)
+        {
+            isShort = false;
+        }
+        else if (thenOrArrow is EqualGreater)
+        {
+            isShort = true;
+        }
+        else
+        {
+            throw new UnexpectedTokenException($"Expected if statement body start, got {thenOrArrow}");
+        }
+
+        token = lexer.GetNextToken();
+        children.Add(NodeFactory.ConstructNode(new BodyNode(), lexer, token, out var elseOrEnd));
+
+        if (elseOrEnd is StatementSeparator)
+        {
+            lastToken = elseOrEnd;
+            return;
+        }
+
+        if (elseOrEnd is Else && isShort)
+        {
+            throw new UnexpectedTokenException("No else allowed on short if.");
+        }
+
+        if (elseOrEnd is not StatementSeparator && elseOrEnd is not Else)
+        {
+            throw new UnexpectedTokenException($"Expected else or statement end. Got {elseOrEnd}");
+        }
+        
+        token = lexer.GetNextToken();
+        children.Add(NodeFactory.ConstructNode(new BodyNode(), lexer, token, out var end));
+
+        if (end is not End)
+        {
+            throw new UnexpectedTokenException($"Expected end of if. Got {end}");
+        }
+
+        lastToken = end;
     }
 }
